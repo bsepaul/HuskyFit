350e8e1bcad0058555b7f5e23e352f7f8fdb8c6e
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-28 19:26:29 -0500

    few changes trying to integrate apis

---
+++ UConnFitApp/shell.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell rec {
+    name = "webdev";
+
+    buildInputs = with pkgs; [
+      # nodePackages.serverless
+      # glow
+      nodejs-16_x
+      # android-studio
+      # android-tools
+      # android-udev-rules
+      # gradle
+      nodePackages.expo-cli
+      # awscli
+      (yarn.override {nodejs = nodejs-16_x;})
+      # (python39.withPackages (pp:
+      #   with pp; [
+      #     beautifulsoup4
+      #     requests
+      #   ]))
+    ];
+  }
