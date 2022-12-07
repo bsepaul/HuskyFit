c6cd67d47d92a0780063d93e5f8a84ad5d8518ef
ari <ariannahblack@gmail.com>
2022-12-06 21:51:18 -0500

    add greeting template to home screen

--- UConnFitApp/navigation/screens/HomeScreen.js
+++ UConnFitApp/navigation/screens/HomeScreen.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Platform, ScrollView } from 'react-native';
 import { myColors } from '../../assets/colors/ColorPalette';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Home Screen!</Text>
+  <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={styles.greetingsContainer}>
+        <Text style={styles.greetingsText}>Hello, Jonathan!</Text>
+        <Text style={{fontSize: 18, paddingVertical: 8, paddingHorizontal: 20}}>Friday, December 9</Text>
+      </View>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </ScrollView>
+  </SafeAreaView>
   );
 }
 
@@ -15,8 +20,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: myColors.lightGrey,
-    alignItems: 'center',
-    justifyContent: 'center',
+      /*alignItems: 'center',*/
+    /*justifyContent: 'center',*/
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+greetingsContainer: {
+    flex: 1,
+    padding: 10
+},
+greetingsText: {
+    width: '100%',
+    height: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    alignItems: 'left',
+    justifyContent: 'left',
+    fontWeight: 'bold',
+    fontSize: 25
+}
 });
