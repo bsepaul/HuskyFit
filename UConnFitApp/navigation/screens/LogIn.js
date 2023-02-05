506860a08047cf33a91972eb9a26827617c7e150
noahramdial57 <noahramdial57@gmail.com>
2023-02-05 13:38:10 -0500

    Added API Integration for Login and Register User

--- UConnFitApp/navigation/screens/LogIn.js
+++ UConnFitApp/navigation/screens/LogIn.js
@@ -1,92 +1,119 @@
-import { useNavigation } from '@react-navigation/native';
-// import React from 'react'
-import React, { useState } from 'react'
-import {View, Text, SafeAreaView, TextInput, TouchableOpacity, Image} from 'react-native';
-import CustomButton from '../../assets/Components/CustomButton';
-import fetch from 'node-fetch'
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity} from "react-native";
+import CustomButton from "../../assets/Components/CustomButton";
+import fetch from "node-fetch";
+import { myColors } from "../../assets/colors/ColorPalette";
 
-import InputField from '../../assets/Components/InputField';
-import { myColors } from '../../assets/colors/ColorPalette';
+let styles = {
+    flexDirection: "row",
+    borderBottomColor: myColors.grey,
+    borderBottomWidth: 1,
+    paddingBottom: 12,
+    marginBottom: 25,
+  };
 
 const Login = ({ navigation }) => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const handleLogin = () => {
-        (async () => {
-            var raw = JSON.stringify({
+  const handleEmail = (text) => {
+    setEmail(text);
+  };
+  const handlePassword = (text) => {    
+    setPassword(text);
+  };
 
-                // "name": "hello world",
-                // "email": "test6@test6.com",
-                // "password": "Password!1",
-                // "birthdate": "12-12-1971"
-                "email": "test2@test2.com",
-                "password": "Password!1",
-                // "email": email,
-                // "password": password,
-              });
+  const handleLogin = () => {
+    // Calling API here
+    (async () => {
+      var raw = JSON.stringify({
+        email: email,
+        password: password,
+      });
 
+      var requestOptions = {
+        method: "POST",
+        headers: {
+          "x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj",
+          "Content-Type": "application/json",
+        },
+        body: raw,
+        redirect: "follow",
+      };
+      fetch(
+        "https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/auth/login",
+        requestOptions
+      )
+      .then(response => response.text())
+      .then((result) => {
+        // Do stuff here
+        var json = JSON.parse(result);
+        var message = json.message // "message": "Success"
+        var token = json.token     // "token": "..."
 
-            var requestOptions = {
-            method: 'POST',
-            headers: {"x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj",
-                      "Content-Type": "application/json"},
-            body: raw,
-            redirect: 'follow'
-            };
-            fetch("https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/auth/login", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-        })()
-        navigation.navigate('Main')
-    }
-    return (
-        <SafeAreaView style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-            <View style= {{paddingHorizontal:25}}>
-                <Text style = {{fontFamily: 'System', fontSize:28, 
-                    fontWeight: '500', color: myColors.navy, marginBottom:30}}> 
-                    Login
-                </Text>
+        console.log(message)
+        console.log()
+        console.log(token)
+        
+        navigation.navigate("Main");
+      })
+      .catch(error => console.log('error', error));
+    })();
+  };
+  return (
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <View style={{ paddingHorizontal: 25 }}>
+        <Text
+          style={{
+            fontFamily: "System",
+            fontSize: 28,
+            fontWeight: "500",
+            color: myColors.navy,
+            marginBottom: 30,
+          }}
+        >
+          Login
+        </Text>
 
-                <InputField label ={'Email Address'} keyboardType = 'email-address' />
-                <InputField label ={'Password'} inputType = 'password' 
-                fieldButtonLabel = {'Forgot?'}
-                fieldButtonFunction = {() => {}}
-                />
+        <View style={styles}>
 
-                <CustomButton label={'Login'} onPress = {handleLogin}/>
-                <View style={{flexDirection:'row', justifyContent:'center', marginBottom: 30}}>
-                    <Text style={{ color:myColors.navy}}> New to the App? </Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                        <Text style={{ color:myColors.navy, fontWeight:'700'}}>  Register </Text>
-                    </TouchableOpacity>
-                </View>
-{/* 
-                <Text style = {{textAllign: 'center', color: myColors.darkGrey, marginbottom: 30 }}> 
-                Or, Login with ... 
-                </Text>
-                <View>
-                    <TouchableOpacity onPress={() => {}} 
-                        style={{
-                            borderColor: myColors.grey, 
-                            borderWidth:2, 
-                            borderRadius:10, 
-                            paddingHorizontal:10, 
-                            paddingVertical: 10,
-                        }}> 
-                        <Image
-                            source={require('../../assets/icons/Google.png')}
-                            resizeMode='contain'
-                            style={{
-                                width: 25,
-                                height: 25
-                            }} />
-                    </TouchableOpacity>
-                </View>  */}
-            </View>
-        </SafeAreaView>
-    );
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+        <View style={styles}>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+
+        <CustomButton label={"Login"} onPress={handleLogin} />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Text style={{ color: myColors.navy }}> New to the App? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text style={{ color: myColors.navy, fontWeight: "700" }}>
+              {" "}
+              Register{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
-export default Login
+export default Login;
--- UConnFitApp/navigation/screens/Register.js
+++ UConnFitApp/navigation/screens/Register.js
@@ -1,98 +1,174 @@
-// import React from 'react'
-import React, { useState } from 'react'
-import fetch from 'node-fetch'
-import {View, Text, SafeAreaView, TextInput, TouchableOpacity, Image, ScrollView} from 'react-native'
-{/*import MaterialIcons from 'react-native-vector-icons/MaterialIcons';*/}
-{/*import MaterialIcons from 'react-native-vector-icons/Ionicons'*/}
-{/*import GooglePNG from './assets/icons/Google.png'; */}
-import InputField from '../../assets/Components/InputField';
-import CustomButton from '../../assets/Components/CustomButton';
-import { myColors } from '../../assets/colors/ColorPalette';
+import React, { useState } from "react";
+import fetch from "node-fetch";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
+import CustomButton from "../../assets/Components/CustomButton";
+import { myColors } from "../../assets/colors/ColorPalette";
 
-
-const Register = ({ navigation }) => {
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [dob, setDob] = useState('')
-
-    const handleRegister = () => {
-        (async () => {
-            var raw = JSON.stringify({
-                // "name": "hello world",
-                // "email": "test6@test6.com",
-                // "password": "Password!1",
-                // "birthdate": "12-12-1971"
-                "name": fullName,
-                "email": email,
-                "password": password,
-                "birthdate": dob
-              });
-
-
-            var requestOptions = {
-            method: 'POST',
-            headers: {"x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj",
-                      "Content-Type": "application/json"},
-            body: raw,
-            redirect: 'follow'
-            };
-
-
-            fetch("https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/auth/signup", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-
-
-        })()
-
-        navigation.navigate('Main')
-    }
-    return (
-        <SafeAreaView style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-            <ScrollView showsVerticalScrollIndicator={false} style= {{paddingHorizontal:25}}>
-                <Text
-                    style={{
-                        fontFamily: 'System',
-                        fontSize: 28, 
-                        fontWeight: '500',
-                        color: myColors.navy,
-                        marginTop: 100,
-                        marginBottom: 1
-                    }}> 
-                    Register
-                </Text>
-                <View
-                    style={{
-                        flexDirection:'row',
-                        marginBottom:25, 
-                        }}>
-                </View>
-
-                <InputField label ={'Full Name'} />
-                <InputField label ={'Email Address'} keyboardType = 'email-address' />
-                <InputField label ={'Password'} inputType = 'password' />
-                <InputField label ={'Confirm Password'} inputType = 'password' />
-                <InputField label ={'Date of Birth'} inputType = 'date' />
-
-                <CustomButton label={'Register'} onPress={handleRegister} /> 
-
-                <View style={{flexDirection:'row', justifyContent:'center', marginBottom: 30}}>
-                    <Text style={{ color:myColors.navy}}> Already Registered? </Text>
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Text style={{ color:myColors.navy, fontWeight:'700'}}>  Login </Text>
-                    </TouchableOpacity>
-                </View>
-
-                {/* <Text style = {{textAllign: 'center', color: '#666', marginbottom: 30 }}> 
-                    Or, Register with ... 
-                </Text>   */}
-
-            </ScrollView>
-        </SafeAreaView>
-);
+let styles = {
+  flexDirection: "row",
+  borderBottomColor: myColors.grey,
+  borderBottomWidth: 1,
+  paddingBottom: 12,
+  marginBottom: 25,
 };
 
-export default Register
+const Register = ({ navigation }) => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [dob, setDob] = useState("");
+
+  const handleFullName = (text) => {
+    setFullName(text);
+  };
+  const handleEmail = (text) => {
+    setEmail(text);
+  };
+  const handlePassword = (text) => {
+    setPassword(text);
+  };
+  // const handleConfirmPassword = (text) => {
+  //   setConfirmPassword(text);
+  // };
+  const handleDob = (text) => {
+    setDob(text);
+  };
+
+  const handleRegister = () => {
+    (async () => {
+      var raw = JSON.stringify({
+        name: fullName,
+        email: email,
+        password: password,
+        birthdate: dob,
+      });
+
+      var requestOptions = {
+        method: "POST",
+        headers: {
+          "x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj",
+          "Content-Type": "application/json",
+        },
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/auth/signup",
+        requestOptions
+      )
+      .then(response => response.text())
+      .then((result) => {
+        // Do stuff here
+        var json = JSON.parse(result);
+        var message = json.message
+        console.log(message) // "User registration succesful"
+        
+        navigation.navigate("Login");
+      })
+      .catch(error => console.log('error', error));
+    })();
+  };
+  return (
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ paddingHorizontal: 25 }}
+      >
+        <Text
+          style={{
+            fontFamily: "System",
+            fontSize: 28,
+            fontWeight: "500",
+            color: myColors.navy,
+            marginTop: 100,
+            marginBottom: 1,
+          }}
+        >
+          Register
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            marginBottom: 25,
+          }}
+        ></View>
+
+        <View style={styles}>
+          <TextInput
+            placeholder="Full Name"
+            placeholderTextColor="#003f5c"
+            onChangeText={(fullName) => setFullName(fullName)}
+          />
+        </View>
+
+        <View style={styles}>
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+
+        <View style={styles}>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+
+        <View style={styles}>
+          <TextInput
+            placeholder="Confirm Password"
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(confirmPassword) =>
+              setConfirmPassword(confirmPassword)
+            }
+          />
+        </View>
+
+        <View style={styles}>
+          <TextInput
+            placeholder="Date of Birth"
+            placeholderTextColor="#003f5c"
+            onChangeText={(dob) => setDob(dob)}
+          />
+        </View>
+
+        <CustomButton label={"Register"} onPress={handleRegister} />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Text style={{ color: myColors.navy }}> Already Registered? </Text>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text style={{ color: myColors.navy, fontWeight: "700" }}>
+              {" "}
+              Login{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Register;
