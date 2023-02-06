103b080da8e27b246963d00fa013d6022537405b
Bridget Smith Epaul <bridget.smith_epaul@uconn.edu>
2023-02-05 23:48:35 -0500

    Adding InputField back in

---
+++ UConnFitApp/assets/Components/InputField.js
@@ -0,0 +1,36 @@
+import {View, Text, TouchableOpacity, TextInput} from 'react-native'
+import React, {useState} from 'react'
+import { myColors } from '../colors/ColorPalette';
+
+export default function InputField({ label, icon, inputType, keyboardType, fieldButtonLabel, fieldButtonFunction }) {
+    return (
+        <View style={{
+            flexDirection:'row', 
+            borderBottomColor: myColors.grey, 
+            borderBottomWidth:1, 
+            paddingBottom: 12, 
+            marginBottom:25, 
+            }}>
+        
+            {icon}
+            {inputType =='password' ? (
+                <TextInput
+                    placeholder = {label}
+                    keyboardType ={keyboardType}
+                    style={{flex: 1, paddingVertical: 0}}
+                    secureTextEntry={true}
+                />
+            ) : (
+                <TextInput 
+                    placeholder = {label}
+                    keyboardType={keyboardType}
+                    secureTextEntry={false}
+                />
+            )}
+
+            <TouchableOpacity onPress={({fieldButtonFunction}) => {}}>
+                <Text style = {{color: myColors.navy, fontWeight: '700'}}> {fieldButtonLabel}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
