9504b7c85dbce893cdb5e972d538d80aa1bad499
Madison Sargeant <madison.sargeant@uconn.edu>
2023-02-07 08:52:36 -0500

    Options Placed in Dropdown

--- UConnFitApp/navigation/screens/WorkoutInfo.js
+++ UConnFitApp/navigation/screens/WorkoutInfo.js
@@ -4,26 +4,68 @@ import InputField from '../../assets/Components/InputField';
 import { myColors } from '../../assets/colors/ColorPalette';
 import { NavigationHelpersContext } from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
+import { TextInput } from 'react-native';
 import {
   Alert,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
+  View 
 } from 'react-native';
 import {Dropdown} from 'react-native-element-dropdown';
 
+const WorkoutTypeData = [
+  { label: 'High Intensity Interval Training', value: '1' },
+  { label: 'Stair Stepper', value: '2' },
+  { label: 'Jump Rope', value: '3' },
+  { label: 'Core Training', value: '4' },
+  { label: 'Dance', value: '5' },
+  { label: 'Eliptical', value: '6' },
+  { label: 'Rower', value: '7' },
+  { label: 'Pilates', value: '8' },
+  { label: 'Basketball', value: '9' },
+  { label: 'Volleyball', value: '10' },
+  { label: 'Soccer', value: '11' },
+  { label: 'Badmitten', value: '12' },
+  { label: 'Raquetball', value: '13' },
+];
+
+const TimeElapsedData = [
+  { label: '0-10 minutes', value: '1' },
+  { label: '10-20 minutes', value: '2' },
+  { label: '20-30 minutes', value: '3' },
+  { label: '30-40 minutes', value: '4' },
+  { label: '40-50 minutes', value: '5' },
+  { label: '50-60 minutes', value: '6' },
+  { label: '60-70 minutes', value: '7' },
+  { label: '70-80 minutes', value: '8' },
+  { label: '80-90 minutes', value: '9' },
+  { label: '90-100 minutes', value: '10' },
+  { label: '100-110 minutes', value: '11' },
+  { label: '110-120 minutes', value: '12' },
+];
+
+const WorkoutIntensityData = [
+  { label: 'Low', value: '1' },
+  { label: 'Medium', value: '2' },
+  { label: 'High', value: '3' },
+];
+
 const App = ({navigation}) => {
-  const [WorkoutTypeData, setWorkoutTypeData] = useState([]);
+  //const [WorkoutTypeData, setWorkoutTypeData] = useState([]);
   const [WorkoutType, setWorkoutType] = useState([]);
-  const [TimeElapsedData, setTimeElapsedData] = useState([]);
+  //const [TimeElapsedData, setTimeElapsedData] = useState([]);
   const [TimeElapsed, setTimeElapsed] = useState([]);
-  const [WorkoutIntensityData, setWorkoutIntensityData] = useState([]);
+  //const [WorkoutIntensityData, setWorkoutIntensityData] = useState([]);
   const [WorkoutIntensity, setWorkoutIntensity] = useState([]);
   const [CalorieData, setSetCalorieData] = useState(null);
   const [Calorie, setSetCalorie] = useState(null);
+  const [CaloriesBurned, setCaloriesBurned] = React.useState("");
   const [isFocus, setIsFocus] = useState(false);
+  const handleCaloriesBurned = (text) => {
+    setCaloriesBurned(text);
+  };
 
 
   return (
@@ -42,8 +84,7 @@ const App = ({navigation}) => {
           labelField="label"
           valueField="value"
           placeholder={!isFocus ? 'Select Workout Type' : '...'}
-          searchPlaceholder="Search..."
-
+          searchPlaceholder="Select One"
           onFocus={() => setIsFocus(true)}
           onBlur={() => setIsFocus(false)}
           onChange={item => {
@@ -94,8 +135,8 @@ const App = ({navigation}) => {
             setIsFocus(false);
           }}
         />
-
-        <InputField label ={'Calories Burned (Optional)'} inputType = 'calories-burned' 
+ 
+ <InputField label ={'Calories Burned (Optional)'} inputType = 'calories-burned' 
                 fieldButtonFunction = {() => {}}
                 /> 
 
@@ -106,9 +147,8 @@ const App = ({navigation}) => {
             borderRadius: 15,
             alignItems: 'center',
           }}
-          onPress={() => navigation.navigate('WorkoutScreen')
-             
-          }>
+          onPress={() => navigation.navigate('WorkoutScreen')}>
+
           <Text
             style={{
               color: '#fff',
