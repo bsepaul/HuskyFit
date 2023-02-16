4c14683f3385c67be40b9370f6b1830566cc3399
ari <ariannahblack@gmail.com>
2023-02-16 14:45:04 -0500

    Added current date to home screen

--- UConnFitApp/navigation/screens/HomeScreen.js
+++ UConnFitApp/navigation/screens/HomeScreen.js
@@ -3,12 +3,21 @@ import { StyleSheet, Text, View, Image, SafeAreaView, Platform, ScrollView } fro
 import { myColors } from '../../assets/colors/ColorPalette';
 
 export default function App() {
+    // Get the day of the week
+    const date = new Date();
+    
+    const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+    const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+    
+    const currentWeekDay = String(days[date.getDay()])
+    const currentMonth = String(months[date.getMonth()])
+    const currentDate = String(date.getDate())
   return (
   <SafeAreaView style={styles.container}>
     <ScrollView>
       <View style={styles.greetingsContainer}>
         <Text style={styles.greetingsText}>Hello, Jonathan!</Text>
-        <Text style={{fontSize: 18, paddingVertical: 8, paddingHorizontal: 20}}>Friday, December 9</Text>
+          <Text style={{fontSize: 18, paddingVertical: 8, paddingHorizontal: 20}}>{currentWeekDay}, {currentMonth} {currentDate}</Text>
       </View>
       <StatusBar style="auto" />
     </ScrollView>
