b57f30b8cf605d6b8fb462c1bf4094dafcebcb65
ari <ariannahblack@gmail.com>
2023-02-20 16:11:45 -0500

    Added sample line chart and activity ring with dummy data

--- UConnFitApp/navigation/screens/HomeScreen.js
+++ UConnFitApp/navigation/screens/HomeScreen.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, Platform, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Platform, ScrollView, Dimensions } from 'react-native';
 import { myColors } from '../../assets/colors/ColorPalette';
+import { LineChart, ContributionGraph, ProgressChart } from 'react-native-chart-kit';
+//import { MyLineChart } from './HomeScreenChart.js';
+import React from 'react'
+
+
 
 export default function App() {
     // Get the day of the week
@@ -12,6 +17,18 @@ export default function App() {
     const currentWeekDay = String(days[date.getDay()])
     const currentMonth = String(months[date.getMonth()])
     const currentDate = String(date.getDate())
+    
+    // for progress ring
+    const ringData = {
+      labels: ["Swim", "Bike", "Run"], // optional
+      data: [0.4, 0.6, 0.8]
+    };
+    
+    //
+
+  
+            //
+            
   return (
   <SafeAreaView style={styles.container}>
     <ScrollView>
@@ -19,10 +36,40 @@ export default function App() {
         <Text style={styles.greetingsText}>Hello, Jonathan!</Text>
           <Text style={{fontSize: 18, paddingVertical: 8, paddingHorizontal: 20}}>{currentWeekDay}, {currentMonth} {currentDate}</Text>
       </View>
+          <View style={styles.chartContainer}>
+          <Text style={styles.chartLabel}>Activity This Week</Text>
+              <LineChart
+                data={
+                    {
+                        labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
+                        datasets: [
+                    {
+                        data: [830, 762, 810, 700, 723, 493, 677]
+                    },
+                ],
+              }
+                }
+                width={Dimensions.get('window').width-50}
+                height={200}
+                chartConfig={ chartConfig }
+              />
+          
+          <ProgressChart
+            data={ringData}
+            width={Dimensions.get('window').width-50}
+            height={220}
+            strokeWidth={16}
+            radius={32}
+            chartConfig={chartConfig}
+            hideLegend={false}
+          />
+          
+          </View>
       <StatusBar style="auto" />
     </ScrollView>
   </SafeAreaView>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -46,5 +93,30 @@ greetingsText: {
     justifyContent: 'left',
     fontWeight: 'bold',
     fontSize: 25
+},
+chartContainer: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+},
+chartLabel: {
+    height: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 50,
+    alignItems: 'center',
+//    fontWeight: 'bold',
+    fontSize: 20
 }
+
 });
+
+const chartConfig = {
+    backgroundGradientFrom: "#E7E7E7",
+//    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#E7E7E7",
+//    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 3) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2,     // optional, default 3
+    barPercentage: 0.5,  // what does this do?
+}
+
