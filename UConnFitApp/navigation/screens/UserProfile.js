83aae2725f35a01dca69e30ba0f20bea43aa70c8
Madison Sargeant <madison.sargeant@uconn.edu>
2023-02-27 20:27:23 -0500

    Tweaking profile screen

--- UConnFitApp/navigation/screens/UserProfile.js
+++ UConnFitApp/navigation/screens/UserProfile.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image, SafeAreaView, Platform, Dimensions} from
 import { myColors } from '../../assets/colors/ColorPalette';
 import CustomButton from "../../assets/Components/CustomButton";
 
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -14,32 +15,31 @@ const UserProfile = ({ navigation }) => {
   const token = route.params.token;
 
   return (
-    <SafeAreaView style={styles.container}>
-     <Text style= {styles.title}>Profile</Text>
+    <SafeAreaView style={{ flex: 1, flexDirection: 'center', justifyContent: 'center', alignContent: 'center' }}>
+      <Text style= {styles.title}>Profile</Text>
       <StatusBar style="auto" />
-
       <CustomButton label={"Food Log"} onPress={() => navigation.navigate('Foodlog', {token:token})}></CustomButton>
       <CustomButton label={"Workout Log"} onPress={() => navigation.navigate('Workoutlog', {token:token})}></CustomButton>
       <CustomButton label={"Personal Survey"} onPress={() => navigation.navigate('Survey', {token:token})}></CustomButton>
       <CustomButton label={"Settings"} onPress={() => navigation.navigate('Settings', {token:token})}></CustomButton>
-
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   buttonRow: {
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: windowWidth*0.15,
+    paddingHorizontal: windowWidth*.25,
   },
   title: {
     fontFamily: "System",
     fontSize: 30,
     fontWeight: "500",
     color: myColors.navy,
-    paddingHorizontal: windowWidth * 0.15,
-    paddingVertical: 15,
+    paddingHorizontal: windowWidth * 0.40,
+    paddingVertical: 20,
   }
 });
+
 export default UserProfile;
