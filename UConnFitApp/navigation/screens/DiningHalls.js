1cb882a27e2268bcfd0bc159b53e46bc72a7a9d4
Bridget Smith Epaul <bridget.smith_epaul@uconn.edu>
2023-03-01 22:26:18 -0500

    Adding date button functionality

--- UConnFitApp/navigation/screens/DiningHalls.js
+++ UConnFitApp/navigation/screens/DiningHalls.js
@@ -10,38 +10,10 @@ const windowHeight = Dimensions.get('window').height;
 
 const DiningHalls = ({ navigation }) => {
   
-  // get dates of yesterday, today and tomorrow
-  var yesterday = new Date();
-  var today = new Date();
-  var tomorrow = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  // convert dates into eastern time zone
-  const yesterdayEST = yesterday.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
-  const todayEST     = today.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
-  const tomorrowEST  = tomorrow.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
-
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-  // get month string for yesterday, today and tomorrow
-  const yesterdayMonth = months[parseInt(yesterdayEST[0]) - 1];
-  const todayMonth = months[parseInt(todayEST[0]) - 1];
-  const tomorrowMonth = months[parseInt(tomorrowEST[0]) - 1];
-
-  // get day string of yesterday, today and tomorrow
-  const yesterdayDate = yesterdayEST[1];
-  const todayDate = todayEST[1];
-  const tomorrowDate = tomorrowEST[1];
-  
   // get current day of the week (buckley is closed on weekends, and only brunch is served on weekends - no breakfast)
+  var today = new Date();
   const day = today.getDay();
   const isWeekend = day === 6 || day === 0;
-
-  // make state variables so we can check the current selected button and pass it onto MealScreen
-  const [isYesterday, setIsYesterday] = React.useState(false);
-  const [isToday, setIsToday] = React.useState(true);
-  const [isTomorrow, setIsTomorrow] = React.useState(false);
 
   // Get token from route
   const route = useRoute();
@@ -51,50 +23,6 @@ const DiningHalls = ({ navigation }) => {
     <SafeAreaView style={styles.screen}>
       <ScrollView contentContainerStyle={styles.content}>
         <Text style={styles.title}>{'Dining Halls'}</Text>
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity
-            style={{
-              backgroundColor: (isYesterday ? myColors.navy : myColors.lightGrey),
-              marginHorizontal: 10,
-              marginTop: 10,
-              width: 65,
-              paddingVertical: 15,
-              borderRadius: 15,
-              alignItems: 'center',
-            }}
-            onPress={() => { setIsYesterday(true); setIsToday(false); setIsTomorrow(false); }}>
-            <Text style={styles.month}>{yesterdayMonth}</Text>
-            <Text  style={styles.day}>{yesterdayDate}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: (isToday ? myColors.navy : myColors.lightGrey),
-              marginHorizontal: 10,
-              marginTop: 10,
-              width: 65,
-              paddingVertical: 15,
-              borderRadius: 15,
-              alignItems: 'center',
-            }}
-            onPress={() => { setIsYesterday(false); setIsToday(true); setIsTomorrow(false); }}>
-            <Text style={styles.month}>{todayMonth}</Text>
-            <Text  style={styles.day}>{todayDate}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: (isTomorrow ? myColors.navy : myColors.lightGrey),
-              marginHorizontal: 10,
-              marginTop: 10,
-              width: 65,
-              paddingVertical: 15,
-              borderRadius: 15,
-              alignItems: 'center',
-            }}
-            onPress={() => { setIsYesterday(false); setIsToday(false); setIsTomorrow(true); }}>
-            <Text style={styles.month}>{tomorrowMonth}</Text>
-            <Text  style={styles.day}>{tomorrowDate}</Text>
-          </TouchableOpacity>
-        </View>
         {isWeekend ? <View></View> : <CustomDiningButton label={'Buckley'} arrow={"right"} onPress={() => navigation.navigate('MealScreen', { dininghall: 'buckley' })} />}
         <CustomDiningButton label={'Gelfenbien'} arrow={"right"} onPress={() => navigation.navigate('MealScreen', { token: token, dininghall : 'gelfenbien' })}/>
         <CustomDiningButton label={'McMahon'}    arrow={"right"} onPress={() => navigation.navigate('MealScreen', { token: token, dininghall : 'mcmahon' })}/>
@@ -126,25 +54,6 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     color: myColors.navy,
   },
-  month: {
-    fontFamily: 'System',
-    fontSize: 16,
-    color: myColors.white,
-  },
-  day: {
-    fontFamily: 'System',
-    fontSize: 28,
-    color: myColors.white,
-    fontWeight: "600",
-  },
-  dateButton: {
-    marginHorizontal: 10,
-    marginTop: 10,
-    width: 65,
-    paddingVertical: 15,
-    borderRadius: 15,
-    alignItems: 'center',
-  }
 });
 
 export default DiningHalls
--- UConnFitApp/navigation/screens/MealScreen.js
+++ UConnFitApp/navigation/screens/MealScreen.js
@@ -2,6 +2,7 @@ import { useRoute } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, View, SafeAreaView, TouchableOpacity, Text, ScrollView, Dimensions, useEffect } from 'react-native';
 import { myColors } from '../../assets/colors/ColorPalette';
+import { ChevronLeft } from "react-native-feather";
 import CustomDiningButton from '../../assets/Components/CustomDiningButton'; 
 import CustomFoodItemButton from '../../assets/Components/CustomFoodItemButton';
 
@@ -23,16 +24,48 @@ const MealScreen = ({ navigation }) => {
   const token = route.params.token;
   const diningHallName = route.params.dininghall.charAt(0).toUpperCase() + route.params.dininghall.slice(1)
   
-
   // Set state variables for condition of showing menus (hide menu if false, show if true)
   const [showBreakfast, setBreakfast] = React.useState(false)
   const [showLunch, setLunch]         = React.useState(false)
-  const [showDinner, setDinner]       = React.useState(false)
+  const [showDinner, setDinner] = React.useState(false)
+  
+  // get dates of yesterday, today and tomorrow
+  var yesterday = new Date();
+  var today = new Date();
+  var tomorrow = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  // convert dates into eastern time zone
+  const yesterdayEST = yesterday.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
+  const todayEST     = today.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
+  const tomorrowEST = tomorrow.toLocaleString('en-US', { timeZone: 'America/New_York' }).split("/");
+  
+  const yesterdayDate = yesterday.toLocaleString('en-US', { timeZone: 'America/New_York' }).split(',')[0];
+  const todayDate = today.toLocaleString('en-US', { timeZone: 'America/New_York' }).split(',')[0];
+  const tomorrowDate = tomorrow.toLocaleString('en-US', { timeZone: 'America/New_York' }).split(',')[0];
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  // get month string for yesterday, today and tomorrow
+  const yesterdayMonth = months[parseInt(yesterdayEST[0]) - 1];
+  const todayMonth = months[parseInt(todayEST[0]) - 1];
+  const tomorrowMonth = months[parseInt(tomorrowEST[0]) - 1];
+
+  // get day string of yesterday, today and tomorrow
+  const yesterdayDay = yesterdayEST[1];
+  const todayDay = todayEST[1];
+  const tomorrowDay = tomorrowEST[1];
   
   // Set state variables for the list of foods collected from api call
   const [breakfastFoods, setBreakfastFoods] = React.useState([])
   const [lunchFoods, setLunchFoods]         = React.useState([])
   const [dinnerFoods, setDinnerFoods]       = React.useState([])
+  
+  // make state variables so we can check the current selected button and pass it onto MealScreen
+  const [isYesterday, setIsYesterday] = React.useState(false);
+  const [isToday, setIsToday] = React.useState(true);
+  const [isTomorrow, setIsTomorrow] = React.useState(false);
 
   var i = 0
 
@@ -57,12 +90,18 @@ const MealScreen = ({ navigation }) => {
     }
   }
 
-  const getMeal = async (meal) => {
+  const getMeal = async (meal, date) => {
     // Make API call based on dining hall name and meal selected
     var fetch_str = "https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/" + route.params.dininghall + "/" + meal;
+    
+    var raw = JSON.stringify({
+      "Date": date // mm/dd/yyyy
+    });
+
     var requestOptions = {
-      method: 'GET',
+      method: 'POST',
       headers: { "x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj" },
+      body: raw,
       redirect: 'follow'
     };
 
@@ -92,17 +131,89 @@ const MealScreen = ({ navigation }) => {
     navigation.navigate('NutritionScreen', { token: token, food: food, breakfastFoods: breakfastFoods, lunchFoods: lunchFoods, dinnerFoods: dinnerFoods, dininghall: diningHallName })
   }
 
-  // Automatically get all meals from API on screen load
+  const updateFood = (selection, date) => {
+
+    if (selection === "yesterday") {
+      setIsYesterday(true);
+      setIsToday(false);
+      setIsTomorrow(false);
+    } else if (selection === "today") {
+      setIsYesterday(false);
+      setIsToday(true);
+      setIsTomorrow(false);
+    } else if (selection === "tomorrow") {
+      setIsYesterday(false);
+      setIsToday(false);
+      setIsTomorrow(true);
+    }
+    console.log("in updateFood");
+    console.log(date);
+    getMeal('breakfast', date);
+    getMeal('lunch', date);
+    getMeal('dinner', date);
+  }
+
+  // Automatically get all meals for today from API on screen load
   React.useEffect(() => {
-    getMeal('breakfast');
-    getMeal('lunch');
-    getMeal('dinner');
+    updateFood("today", todayDate);
   }, []);
 
   return (
-    <SafeAreaView style={styles.screen}>
-      <ScrollView contentContainerStyle={styles.content }>
-        <Text style={styles.title}>{diningHallName}</Text>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.content}>
+        <View style={{flexDirection:'row', justifyContent: 'flex-start', alignContent:'center', paddingRight:46}}>
+            <TouchableOpacity
+                style={{ paddingHorizontal: 5 }}
+                onPress={() => navigation.navigate('DiningHalls', { token: token, dininghall: route.params.dininghall })} >
+              <ChevronLeft stroke={myColors.navy} strokeWidth={2} width={36} height={36} />
+            </TouchableOpacity>
+            <Text style={styles.title}>{diningHallName}</Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: (isYesterday ? myColors.navy : myColors.lightGrey),
+              marginHorizontal: 10,
+              marginTop: 10,
+              width: 65,
+              paddingVertical: 15,
+              borderRadius: 15,
+              alignItems: 'center',
+            }}
+            onPress={() => updateFood("yesterday", yesterdayDate)}>
+            <Text style={styles.month}>{yesterdayMonth}</Text>
+            <Text  style={styles.day}>{yesterdayDay}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: (isToday ? myColors.navy : myColors.lightGrey),
+              marginHorizontal: 10,
+              marginTop: 10,
+              width: 65,
+              paddingVertical: 15,
+              borderRadius: 15,
+              alignItems: 'center',
+            }}
+            onPress={() => updateFood("today", todayDate)}>
+            <Text style={styles.month}>{todayMonth}</Text>
+            <Text  style={styles.day}>{todayDay}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: (isTomorrow ? myColors.navy : myColors.lightGrey),
+              marginHorizontal: 10,
+              marginTop: 10,
+              width: 65,
+              paddingVertical: 15,
+              borderRadius: 15,
+              alignItems: 'center',
+            }}
+            onPress={() => updateFood("tomorrow", tomorrowDate)}>
+            <Text style={styles.month}>{tomorrowMonth}</Text>
+            <Text  style={styles.day}>{tomorrowDay}</Text>
+          </TouchableOpacity>
+        </View>
+
         {isWeekend ? <View></View> : <CustomDiningButton label={'Breakfast'} arrow={showBreakfast ? "up" : "down"} onPress={() => showMeal('breakfast')} />}
         <View>
           {showBreakfast ?
@@ -119,7 +230,7 @@ const MealScreen = ({ navigation }) => {
                 })}
               </ScrollView>
             </View> :
-            <Text></Text>
+            <View></View>
           }
         </View>
     
@@ -139,7 +250,7 @@ const MealScreen = ({ navigation }) => {
                 })}
               </ScrollView>
             </View> :
-            <Text></Text>
+            <View></View>
           }
         </View>
         
@@ -159,7 +270,7 @@ const MealScreen = ({ navigation }) => {
                 })}
               </ScrollView>
               </View> :
-              <Text></Text>
+              <View></View>
             }
           </View>
 
@@ -174,11 +285,6 @@ const MealScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   content: {
     alignItems: 'center',
     marginHorizontal: windowWidth * 0.125,
@@ -196,10 +302,10 @@ const styles = StyleSheet.create({
     minHeight: 0,
     maxHeight: 250,
     width: windowWidth*0.75,
-    marginBottom: 15,
+    marginTop: 5,
     paddingTop: 8,
     backgroundColor: myColors.lightGrey,
-    borderRadius: 10,
+    borderRadius: 12,
   },
   item: {
     fontFamily: "System",
@@ -207,5 +313,24 @@ const styles = StyleSheet.create({
     color: myColors.navy,
     padding: 5,
   },
+  month: {
+    fontFamily: 'System',
+    fontSize: 16,
+    color: myColors.white,
+  },
+  day: {
+    fontFamily: 'System',
+    fontSize: 28,
+    color: myColors.white,
+    fontWeight: "600",
+  },
+  dateButton: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    width: 65,
+    paddingVertical: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+  }
 });
 export default MealScreen
