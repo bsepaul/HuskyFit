ae59747ece5de8df46d24147f6172030ce1c5549
Bridget Smith Epaul <bridget.smith_epaul@uconn.edu>
2023-03-02 11:09:18 -0500

    Getting user's name from API

--- UConnFitApp/navigation/screens/HomeScreen.js
+++ UConnFitApp/navigation/screens/HomeScreen.js
@@ -3,55 +3,83 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView, Platform, Dimensions } from 'react-native';
 import { myColors } from '../../assets/colors/ColorPalette';
 import { ProgressChart, BarChart } from 'react-native-chart-kit';
-import React from 'react'
+import React from 'react';
+import fetch from 'node-fetch';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function HomeScreen() {
 
+  // Get token from route
+  const route = useRoute();
+  const token = route.params.token;
+  
+  // Set variable to store the user's name once fetched from API
+  const [name, setName] = React.useState('');
 
-    // Get token from route
-    const route = useRoute();
-    const token = route.params.token;
+  // get the day of the week
+  const date = new Date();
+  // convert to weekday, month ## format
+  const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+  const daysAbbrev = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+  const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+  const currentWeekDay = String(days[date.getDay()])
+  const currentMonth = String(months[date.getMonth()])
+  const currentDate = String(date.getDate())
+  
+  // data
+  // for progress ring
+  const ringData = {                  // take from user data, last 3 workouts performed?
+    labels: ["Bike", "Run", "Lift"],
+    data: [0.4, 0.6, 0.8]             // can set default goal calories to 100 per exercise?
+  };
+  // for calorie bars
+  // only show data for days we've already seen this week
+  const barLabels = []
+  console.log(String(date.getDay()))
+  for (let i=0; i <= date.getDay(); i++) {
+    barLabels.push(String(daysAbbrev[i]))
+  }
+    // dummy data
+  const calData = [1750, 1712, 1679, 1712, 1679, 1599, 1650] // change to user data set, exclude 0's for days with no data
 
-    // get the day of the week
-    const date = new Date();
-    // convert to weekday, month ## format
-    const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
-    const daysAbbrev = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
-    const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-    const currentWeekDay = String(days[date.getDay()])
-    const currentMonth = String(months[date.getMonth()])
-    const currentDate = String(date.getDate())
-    
-    // data
-    // for progress ring
-    const ringData = {                  // take from user data, last 3 workouts performed?
-      labels: ["Bike", "Run", "Lift"],
-      data: [0.4, 0.6, 0.8]             // can set default goal calories to 100 per exercise?
-    };
-    // for calorie bars
-    // only show data for days we've already seen this week
-    const barLabels = []
-    console.log(String(date.getDay()))
-    for (let i=0; i <= date.getDay(); i++) {
-      barLabels.push(String(daysAbbrev[i]))
-    }
-      // dummy data
-    const calData = [1750, 1712, 1679, 1712, 1679, 1599, 1650] // change to user data set, exclude 0's for days with no data
-
-    const barData = {
+  const barData = {
     labels: barLabels,
     datasets: [{
-    data: calData.slice(date.getDay())  // only show cal data since last Sunday
+      data: calData.slice(date.getDay())  // only show cal data since last Sunday
     }]
-    }
+  }
+  
+  const getUserInfo = () => {
+    (async () => {
+      var raw = ""
+      var requestOptions = {
+        method: 'GET',
+        headers: {"x-api-key": "baKUvaQPWW2ktAmIofzBz6TkTUmnVcQzX5qlPfEj",
+                  "Authorization": token},
+        body: raw,
+        redirect: 'follow'
+      };
+      fetch("https://ap782aln95.execute-api.us-east-1.amazonaws.com/dev/profile", requestOptions)
+      .then(response => response.text())
+        .then((result) => {
+          var json = JSON.parse(result);
+          setName(json.Name.split(' ')[0])
+      })
+      .catch(error => console.log('error', error));
+    })()
+  }
+
+  // Automatically get all meals for today from API on screen load
+  React.useEffect(() => {
+    getUserInfo();
+  }, []);
       
   return (
   <SafeAreaView style={styles.container}>
       <View style={styles.greetingsContainer}>
-        <Text style={styles.title}>Hello, Jonathan!</Text>
+        <Text style={styles.title}>Hello, {name}</Text>
           <Text style={{fontSize: 20, paddingVertical: 0, paddingHorizontal: 20, color: myColors.navy}}>{currentWeekDay}, {currentMonth} {currentDate}</Text>
       </View>
           <View style={styles.chartContainer}>
