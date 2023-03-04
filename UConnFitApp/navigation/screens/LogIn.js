d01ed3bf0ea8d8c78837bbc46195480d2d3026a7
ari <ariannahblack@gmail.com>
2023-03-03 21:11:17 -0500

    Fixed login functionality

--- UConnFitApp/navigation/screens/LogIn.js
+++ UConnFitApp/navigation/screens/LogIn.js
@@ -60,23 +60,22 @@ const Login = ({ navigation }) => {
         // Do stuff here
         var json = JSON.parse(result);
         var message = json.message // "message": "Success"
-        // var token = json.token     // "token": "..."
-        var token = 'eyJraWQiOiJBd2F0VzNTNlZkUTVkZ3ZYYmRQN1lqQzVLMENoRXc2NnRFK1JlQld0QTJVPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmOGMwZjQ5Yi03NjQ1LTRjMzAtYTQ1NS01MDUzODdkZTAyYzQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYmlydGhkYXRlIjoiMDRcLzE1XC8yMDAxIiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZzM1TmJlc1ZTIiwiY29nbml0bzp1c2VybmFtZSI6ImJyaWRnZXQuc21pdGhfZXBhdWxAdWNvbm4uZWR1Iiwib3JpZ2luX2p0aSI6IjFkYTNiYjMxLTIyYjgtNGYzZC1iMDczLTIyN2I5OGVjNmM2MiIsImF1ZCI6IjN2MDBobXJlMjJ1cWVjdmw5anVjOGdiMTNvIiwiZXZlbnRfaWQiOiJhZjYwY2VhNy1jOGQ4LTRiOTgtOGRmMS1lYzE4MDMzMTc4OTUiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY3Nzg3MzE2MSwibmFtZSI6IkJyaWRnZXQgU21pdGggRXBhdWwiLCJleHAiOjE2Nzc4OTExNjEsImlhdCI6MTY3Nzg3MzE2MSwianRpIjoiNjMwZTA0YjgtN2VhOS00OTFmLTgwODktZmQ5ZmVmODUyMzRkIiwiZW1haWwiOiJicmlkZ2V0LnNtaXRoX2VwYXVsQHVjb25uLmVkdSJ9.obHtZOJttowibpWLsjEBOgcHez7OPc7kgHlyVTitJ3Urqg1k5EKpWWlYAONj6lJP7EsJ8_o7amsdzBaonxGWfRF5u1BLgby8msAktouhqyBsRz6H6bZNc1l2zFf2HS8RVUTUBLnKpy-ZuANHOvnCSVPOlXK-WdHK9GXcZ0GHgSgA-6RqSLS17o-V5Utae_GAuovMEJs7Yny8UkELaJsClve82erZmSUs8blcGSy7TiZDZo1_9oxnob6ENYbzAaG6QR13_axs1-z5ADajCJ5faEJwFDaMitiQQ2cG9q1oGI91ts4fn-yMsqhNonNyhsj8nbvdlqidRTmlJS5YNWTEPg';
-
+        var token = json.token     // "token": "..."
+      
         console.log(message)
         console.log()
         console.log(token)
 
         //If you want to bypass the login credentials, uncomment this
-        navigation.navigate("Tabs", {token: token});
+        // navigation.navigate("Tabs", {token: token});
         
         // If you want to test the login credentials or need the token for testing something, uncomment this
-        // if ((message != "Invalid input") && (message != "User does not exist.")) {
-        //  navigation.navigate("Tabs", {token: token});
-        // }
-        // else {
-        //   alertFailure();
-        // }
+        if ((message != "Invalid input") && (message != "User does not exist.")) {
+         navigation.navigate("Tabs", {token: token});
+        }
+        else {
+          alertFailure();
+        }
         
       })
       .catch(error => console.log('error', error));
